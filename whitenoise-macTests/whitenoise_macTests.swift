f46129fda890d4ee9a7c17f1d54877f4261e0fbe
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2026-05-26 09:41:35 +0200

    Initial Commit

---
+++ whitenoise-macTests/whitenoise_macTests.swift
@@ -0,0 +1,19 @@
+//
+//  whitenoise_macTests.swift
+//  whitenoise-macTests
+//
+//  Created by Jeff Gardner on 26/05/2026.
+//
+
+import Testing
+@testable import whitenoise_mac
+
+struct whitenoise_macTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
