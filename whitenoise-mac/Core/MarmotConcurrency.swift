3825b9113b3bec117884430c846f3e29f23e000c
agent-p1p <agent-p1p@users.noreply.github.com>
2026-06-17 17:30:03 +0200

    fix: move sync ffi reads off main actor

--- whitenoise-mac/Core/MarmotConcurrency.swift
+++ whitenoise-mac/Core/MarmotConcurrency.swift
@@ -9,3 +9,12 @@ extension TimelineMessageQueryFfi: @retroactive @unchecked Sendable {}
 extension TimelinePageFfi: @retroactive @unchecked Sendable {}
 extension ChatListRowFfi: @retroactive @unchecked Sendable {}
 extension AppMessageRecordFfi: @retroactive @unchecked Sendable {}
+extension AccountSummaryFfi: @retroactive @unchecked Sendable {}
+extension UserProfileMetadataFfi: @retroactive @unchecked Sendable {}
+extension AccountRelayListsFfi: @retroactive @unchecked Sendable {}
+extension NotificationSettingsFfi: @retroactive @unchecked Sendable {}
+extension RelayTelemetrySettingsFfi: @retroactive @unchecked Sendable {}
+extension AuditLogSettingsFfi: @retroactive @unchecked Sendable {}
+extension AuditLogFileFfi: @retroactive @unchecked Sendable {}
+extension AuditLogTrackerConfigFfi: @retroactive @unchecked Sendable {}
+extension MemberRefFfi: @retroactive @unchecked Sendable {}
